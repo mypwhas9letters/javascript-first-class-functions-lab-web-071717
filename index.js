c5cadc6996c80bee96c7c084ae2cbd5fa1dbe898
Hui Wang <huiw3939@gmail.com>
2017-08-29 18:40:01 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function countdown(callback){
+  setTimeout(callback, 2000)
+}
+ function createMultiplier(multiplyValue){
+   return function (value){
+     return multiplyValue * value
+   }
+ }
+
+ function multiplier(arg1, arg2){
+   return arg1 * arg2
+ }
+
+
+
+ var doubler = createMultiplier(2);
+ var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null,2)
+var triplerWithBind = multiplier.bind(null,3)
